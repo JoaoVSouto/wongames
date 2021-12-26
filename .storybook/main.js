d29902f6cacef53b6b2b50d8c53015618376ea62
João Vítor <joaovsoutosantos@gmail.com>
2021-12-26 14:36:59 -0300

    chore: configure absolute paths for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,10 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials'
-  ]
+  ],
+  webpackFinal(config) {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+
+    return config
+  }
 }
