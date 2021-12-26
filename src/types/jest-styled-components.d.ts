d9c358e403bd01804c671ce5b13aa8aa27b3b52b
João Vítor <joaovsoutosantos@gmail.com>
2021-12-25 23:53:39 -0300

    Initial commit from Create Next App

---
+++ src/types/jest-styled-components.d.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
+import { Plugin, NewPlugin } from 'pretty-format'
+import { css } from 'styled-components'
+
+declare global {
+  namespace jest {
+    interface AsymmetricMatcher {
+      $$typeof: Symbol
+      sample?: string | RegExp | object | Array<any> | Function
+    }
+
+    type Value = string | number | RegExp | AsymmetricMatcher | undefined
+
+    interface Options {
+      media?: string
+      modifier?: string | ReturnType<typeof css>
+      supports?: string
+    }
+
+    interface Matchers<R> {
+      toHaveStyleRule(property: string, value?: Value, options?: Options): R
+    }
+  }
+}
+
+export interface StyledComponentsSerializerOptions {
+  addStyles?: boolean
+  classNameFormatter?: (index: number) => string
+}
+
+export declare const styleSheetSerializer: Exclude<Plugin, NewPlugin> & {
+  setStyleSheetSerializerOptions: (
+    options?: StyledComponentsSerializerOptions
+  ) => void
+}
