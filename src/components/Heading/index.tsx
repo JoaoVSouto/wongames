ef6b9dcf25f9cf921b542aebd6dacddef2d81e15
João Vítor <joaovsoutosantos@gmail.com>
2021-12-29 11:44:27 -0300

    feat(heading): create variants regarding to line color and size

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -2,20 +2,32 @@ import * as React from 'react'
 
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  lineColor?: LineColors
+  size?: 'small' | 'medium'
 }
 
 export const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  lineColor = 'primary',
+  size = 'medium'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    lineColor={lineColor}
+    size={size}
+  >
     {children}
   </S.Wrapper>
 )
--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -23,7 +23,7 @@ describe('<Heading />', () => {
   it('should render a heading with a line on the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': `0.7rem solid ${theme.colors.secondary}`
+      'border-left': `0.7rem solid ${theme.colors.primary}`
     })
   })
 
@@ -32,6 +32,60 @@ describe('<Heading />', () => {
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
       'border-bottom',
       `0.5rem solid ${theme.colors.primary}`,
+      {
+        modifier: '::after'
+      }
+    )
+  })
+
+  it('should render a heading with a small size', () => {
+    renderWithTheme(<Heading size="small">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '1.6rem'
+    })
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'width',
+      '3rem',
+      {
+        modifier: '::after'
+      }
+    )
+  })
+
+  it('should render a heading with a primary line color', () => {
+    renderWithTheme(
+      <Heading lineLeft lineBottom lineColor="primary">
+        Won Games
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyle({
+      'border-left': `0.7rem solid ${theme.colors.primary}`
+    })
+    expect(heading).toHaveStyleRule(
+      'border-bottom',
+      `0.5rem solid ${theme.colors.primary}`,
+      {
+        modifier: '::after'
+      }
+    )
+  })
+
+  it('should render a heading with a secondary line color', () => {
+    renderWithTheme(
+      <Heading lineLeft lineBottom lineColor="secondary">
+        Won Games
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyle({
+      'border-left': `0.7rem solid ${theme.colors.secondary}`
+    })
+    expect(heading).toHaveStyleRule(
+      'border-bottom',
+      `0.5rem solid ${theme.colors.secondary}`,
       {
         modifier: '::after'
       }
