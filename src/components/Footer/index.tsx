d2c11bd3a6ae52245580b6d9483c66fd739bba8d
João Vítor <joaovsoutosantos@gmail.com>
2021-12-29 12:17:55 -0300

    feat(footer): create component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,88 @@
+import Link from 'next/link'
+
+import { Logo } from 'components/Logo'
+import { Heading } from 'components/Heading'
+
+import * as S from './styles'
+
+export const Footer = () => (
+  <S.Wrapper>
+    <Logo color="black" />
+
+    <S.Content>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Contact us
+        </Heading>
+
+        <a href="mailto:sac@wongames.com">sac@wongames.com</a>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Follow us
+        </Heading>
+
+        <nav aria-label="social media">
+          <a
+            href="https://www.instagram.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.twitter.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.youtube.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.facebook.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Facebook
+          </a>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Links
+        </Heading>
+
+        <nav aria-label="footer resources">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/games">
+            <a>Store</a>
+          </Link>
+          <Link href="/search">
+            <a>Search</a>
+          </Link>
+        </nav>
+      </S.Column>
+
+      <S.Column aria-label="footer contact">
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Location
+        </Heading>
+        <span>Lorem ipsum dolor sit.</span>
+        <span>Lorem Ipsum</span>
+        <span>Lorem, ipsum dolor.</span>
+      </S.Column>
+    </S.Content>
+
+    <S.Copyright>Won Games 2020 © All rights reserved.</S.Copyright>
+  </S.Wrapper>
+)
