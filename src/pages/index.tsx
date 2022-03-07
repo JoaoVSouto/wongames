ce607cb484d852882414696a420216cb870ca34a
João Vítor <joaovsoutosantos@gmail.com>
2022-03-07 20:30:27 -0300

    chore: inject mocks into home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-import { Home } from 'templates/Home'
+import { Home, HomeTemplateProps } from 'templates/Home'
+import { mock as bannersMock } from 'components/BannerSlider/mock'
+import { mock as gamesMock } from 'components/GameCardSlider/mock'
+import { mock as highlightMock } from 'components/Highlight/mock'
 
-export default function Index() {
-  return <Home />
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcomingGames: gamesMock,
+      upcomingHighlight: highlightMock,
+      upcomingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighlight: highlightMock
+    }
+  }
 }
