db17de26f874dbcb9efdd5c87a09d9c87df72bdf
João Vítor <joaovsoutosantos@gmail.com>
2022-03-19 14:35:10 -0300

    feat(checkbox): add onCheck function

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,22 +1,44 @@
+import * as React from 'react'
+
 import * as S from './styles'
 
 export type CheckboxProps = {
   label?: string
   labelFor?: string
   labelColor?: 'white' | 'black'
-}
+  onCheck?: (status: boolean) => void
+} & React.InputHTMLAttributes<HTMLInputElement>
 
 export const Checkbox = ({
   label,
   labelFor = '',
-  labelColor = 'white'
-}: CheckboxProps) => (
-  <S.Wrapper>
-    <S.Input id={labelFor} type="checkbox" />
-    {label && (
-      <S.Label htmlFor={labelFor} labelColor={labelColor}>
-        {label}
-      </S.Label>
-    )}
-  </S.Wrapper>
-)
+  labelColor = 'white',
+  onCheck
+}: CheckboxProps) => {
+  const [checked, setChecked] = React.useState(false)
+
+  const onChange = () => {
+    const status = !checked
+    setChecked(status)
+
+    if (onCheck) {
+      onCheck(status)
+    }
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        checked={checked}
+        onChange={onChange}
+      />
+      {label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,4 +1,5 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { renderWithTheme } from 'utils/tests/helpers'
 import { theme } from 'styles/theme'
@@ -27,6 +28,19 @@ describe('<Checkbox />', () => {
 
     expect(screen.getByText(/checkbox label/i)).toHaveStyle({
       color: theme.colors.black
+    })
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="checkbox" onCheck={onCheck} />)
+
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
     })
   })
 })
