a30a83bd7b352598e92e29053d2850b4a91d2a3e
João Vítor <joaovsoutosantos@gmail.com>
2022-03-26 15:28:52 -0300

    feat(auth): create initial page structure

---
+++ src/templates/Auth/test.tsx
@@ -0,0 +1,37 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import { Auth } from '.'
+
+describe('<Auth />', () => {
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth Title">
+        <input type="text" />
+      </Auth>
+    )
+
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and most complete gaming platform./i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /auth title/i
+      })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+  })
+})
