b9ffc2d0c50e812809352fc86b6e3ba4fbd8f14c
João Vítor <joaovsoutosantos@gmail.com>
2022-03-27 16:50:02 -0300

    feat(sign in form): create initial structure

---
+++ src/components/FormSignIn/stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, Story } from '@storybook/react/types-6-0'
+
+import { FormSignIn } from '.'
+
+export default {
+  title: 'Form/FormSignIn',
+  component: FormSignIn
+} as Meta
+
+export const Basic: Story = args => (
+  <div style={{ width: 300, margin: 'auto' }}>
+    <FormSignIn {...args} />
+  </div>
+)
---
+++ src/components/FormSignIn/test.tsx
@@ -0,0 +1,32 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import { FormSignIn } from '.'
+
+describe('<FormSignIn />', () => {
+  it('should render the form', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /sign in now/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('link', { name: /forgot your password\?/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the text to sign up', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
+    expect(screen.getByText(/don't have an account\?/i)).toBeInTheDocument()
+  })
+})
