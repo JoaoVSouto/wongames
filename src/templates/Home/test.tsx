283a63b80f931ad26c06424b175d7bffb2651337
João Vítor Souto <inv.jsantos@ifood3rd.com>
2022-07-27 20:28:06 -0300

    test(home): increase test performance

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -20,29 +20,33 @@ const props = {
   freeHighlight: highlightMock
 }
 
+jest.mock('components/Menu', () => ({
+  __esModule: true,
+  Menu: () => <div data-testid="Mock Menu" />
+}))
+
+jest.mock('components/Footer', () => ({
+  __esModule: true,
+  Footer: () => <div data-testid="Mock Footer" />
+}))
+
+jest.mock('components/Showcase', () => ({
+  __esModule: true,
+  Showcase: () => <div data-testid="Mock Showcase" />
+}))
+
+jest.mock('components/BannerSlider', () => ({
+  __esModule: true,
+  BannerSlider: () => <div data-testid="Mock BannerSlider" />
+}))
+
 describe('<Home />', () => {
   it('should render menu and footer', () => {
     renderWithTheme(<Home {...props} />)
 
-    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
-    expect(
-      screen.getByRole('heading', { name: /follow us/i })
-    ).toBeInTheDocument()
-    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
-
-    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
-    expect(
-      screen.getByRole('heading', { name: /most popular/i })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole('heading', { name: /upcoming/i })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole('heading', { name: /free games/i })
-    ).toBeInTheDocument()
-
-    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
-    expect(screen.getAllByText(/red dead is back/i)).toHaveLength(3)
+    expect(screen.getByTestId('Mock Menu')).toBeInTheDocument()
+    expect(screen.getByTestId('Mock BannerSlider')).toBeInTheDocument()
+    expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(5)
+    expect(screen.getByTestId('Mock Footer')).toBeInTheDocument()
   })
 })
