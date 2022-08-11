48613ec092f1139e68fc5e72190dad17db8163e2
João Vítor Souto <inv.jsantos@ifood3rd.com>
2022-08-10 22:47:49 -0300

    feat(game page): add game info

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,13 +1,22 @@
 import { Base } from 'templates/Base'
 
+import { GameInfo, GameInfoProps } from 'components/GameInfo'
+
 import * as S from './styles'
 
 export type GameTemplateProps = {
   cover: string
+  gameInfo: GameInfoProps
 }
 
-export const Game = ({ cover }: GameTemplateProps) => (
+export const Game = ({ cover, gameInfo }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
+
+    <S.Main>
+      <S.SectionGameInfo>
+        <GameInfo {...gameInfo} />
+      </S.SectionGameInfo>
+    </S.Main>
   </Base>
 )
